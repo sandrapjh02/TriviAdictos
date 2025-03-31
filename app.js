77d064ea1292d9ab85826bfc4739d308a6379403
Sandra Jimenez <sandrapjh02@gmail.com>
2025-03-31 10:27:52 -0500

    Subiendo a repositori

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+document.querySelector(".star-button").addEventListener("click", () => {
+    alert("¡Botón de estrella presionado!");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.querySelector(".menu-icon");
+    
+    menuIcon.addEventListener("click", () => {
+        alert("Menú desplegable en construcción.");
+    });
+});
